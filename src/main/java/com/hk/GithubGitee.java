bcd297b47a902e3fd2cae279ca8c8c16d6ff8c84
DELL <DELL@DESKTOP-I4IFOS6>
2021-09-23 15:12:00 +0800

    master commit 4

--- src/main/java/com/hk/GithubGitee.java
+++ src/main/java/com/hk/GithubGitee.java
@@ -14,6 +14,7 @@ public class GithubGitee {
         System.out.println("cehsi 100001");
         System.out.println("cehsi 100002");
         System.out.println("cehsi 100003");
+        System.out.println("cehsi 100004");
         System.out.println("cehsi 100002  hot-fix 001");
         System.out.println("cehsi 100002  hot-fix 002");
     }
