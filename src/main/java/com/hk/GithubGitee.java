4c24768c2f3df77d87a29220978127a33cc50307
DELL <DELL@DESKTOP-I4IFOS6>
2021-09-23 14:45:48 +0800

    master commit 1

---
+++ src/main/java/com/hk/GithubGitee.java
@@ -0,0 +1,16 @@
+package com.hk;
+
+/**
+ * @ProjectName: github_gitee
+ * @Package: com.hk
+ * @ClassName: GithubGitee
+ * @Author: HK
+ * @Description:
+ * @Date: 2021/9/23 14:44
+ * @Version: 1.0
+ */
+public class GithubGitee {
+    public static void main(String[] args) {
+        System.out.println("cehsi 100001");
+    }
+}
